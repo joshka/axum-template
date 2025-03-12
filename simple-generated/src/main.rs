c8f0941d120fab90932bb1faf3067f12cafa40fa
Josh McKinney <joshka@users.noreply.github.com>
2025-03-12 10:52:56 -0700

    feat: handle graceful shutdown

--- simple-generated/src/main.rs
+++ simple-generated/src/main.rs
@@ -1,6 +1,9 @@
+use std::{io, time::Duration};
+
 use axum::{routing::get, Router};
 use cli::Cli;
 use color_eyre::Result;
+use tokio::{signal::ctrl_c, task::JoinHandle};
 use tracing::{error, info};
 
 mod cli;
@@ -18,11 +21,8 @@ async fn main() -> Result<()> {
 
 async fn run(args: Cli) -> Result<()> {
     let handle = axum_server::Handle::new();
-    let addr = (args.ip, args.port).into();
-    let config = tls::init(args.data_dir).await?;
-    let server = axum_server::bind_rustls(addr, config).handle(handle.clone());
-    let app = router().into_make_service();
-    let server_task = tokio::spawn(server.serve(app));
+    let server_task = start_service(&args, handle.clone()).await?;
+
     let Some(addr) = handle.listening().await else {
         error!("failed to start server");
         return Ok(());
@@ -34,8 +34,31 @@ async fn run(args: Cli) -> Result<()> {
         webbrowser::open(&addr).ok();
     }
 
+    shutdown_signal().await;
+    handle.graceful_shutdown(Some(Duration::from_secs(10)));
+
     server_task.await??;
     Ok(())
+}
+
+async fn start_service(
+    args: &Cli,
+    handle: axum_server::Handle,
+) -> Result<JoinHandle<io::Result<()>>> {
+    let addr = (args.ip, args.port).into();
+    let config = tls::init(&args.data_dir).await?;
+    let server = axum_server::bind_rustls(addr, config).handle(handle);
+    let app = router().into_make_service();
+    let server_task = tokio::spawn(server.serve(app));
+    Ok(server_task)
+}
+
+async fn shutdown_signal() {
+    match ctrl_c().await {
+        Ok(()) => info!("Received Ctrl-C"),
+        Err(err) => error!("Failed to listen for Ctrl-C: {err}"),
+    }
+    info!("Shutting down");
 }
 
 fn router() -> Router {
